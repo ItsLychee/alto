894fa4e8152d2b2f430ca829d2489b4f0be7c139
itslychee <itslychee@protonmail.com>
2024-03-11 02:45:32 -0500

    project: add Cargo files and nix expressions

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A metadata-based media organizer";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      inherit (builtins) attrValues;
+      eachSystem = f:
+        lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+        (system: f nixpkgs.legacyPackages.${system});
+    in {
+
+      packages = eachSystem (pkgs: rec {
+        default = alto;
+        alto = pkgs.callPackage ./package.nix { };
+      });
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = attrValues { inherit (pkgs) cargo rustc; };
+        };
+      });
+
+      formatter = eachSystem (pkgs: pkgs.nixfmt);
+
+    };
+}
---
+++ package.nix
@@ -0,0 +1,8 @@
+{ rustPlatform }:
+rustPlatform.buildRustPackage {
+  pname = "alto";
+  version = "0-unstable-2024-03-11";
+  src = ./.;
+  cargoLock.lockFile = ./Cargo.lock;
+  meta = { mainProgram = "alto"; };
+}
