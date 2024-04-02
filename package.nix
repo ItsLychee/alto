d40a4c14ae328597188898146b3f8024745f915f
itslychee <itslychee@protonmail.com>
2024-04-01 19:22:07 -0500

    package: don't use an attrset block for mainProgram

--- package.nix
+++ package.nix
@@ -4,5 +4,5 @@ rustPlatform.buildRustPackage {
   version = "0-unstable-2024-03-11";
   src = ./.;
   cargoLock.lockFile = ./Cargo.lock;
-  meta = { mainProgram = "alto"; };
+  meta.mainProgram = "alto";
 }
